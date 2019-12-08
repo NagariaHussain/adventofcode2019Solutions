d242a8ef66680a19cf0e87df8168841fb4200115
Hussain Nagaria <hussainbhaitech@gmail.com>
2019-12-08 17:05:39 +0530

    added day 6 part two solution

--- day6.py
+++ day6.py
@@ -25,3 +25,22 @@ for star in relations.keys():
 print(f'ORBIT CHECKSUM: {orbit_checksum}')
 
 # ------------- PART TWO ----------------
+
+def getParents(star):
+    parents = []
+    while star in relations:
+        star = relations[star]
+        parents.append(star)
+    return parents
+
+you_parents = getParents('YOU')
+san_parents = getParents('SAN')
+
+for p in you_parents:
+    if p in san_parents:
+        intersection = p
+        break
+
+orbits_jump = you_parents.index(intersection) + san_parents.index(intersection)
+
+print(f'ORBITS TO JUMP: {orbits_jump}')
