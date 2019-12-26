c0c94278b291a86558720a815191d54e487f2ed6
NagariaHussain <hussainbhaitech@gmail.com>
2019-12-26 13:02:42 +0530

    day 14 complete

--- day14.py
+++ day14.py
@@ -15,7 +15,6 @@ with open('input14.txt', 'r') as f:
         product  = reaction.rstrip('\n').split(' => ')[1].split(' ')
         product_qty = int(product[0])
         product_name = product[1]
-
         reactions[product_name] = (product_qty, reactants)
     pprint(reactions)
 
@@ -31,13 +30,12 @@ def getCount(chemical, amount):
     global ore_count
     if chemical == 'ORE':
         ore_count += amount
-        return
+        return 
     for reactant, qty in reactions[chemical][1].items():
         times = ceil(amount / reactions[chemical][0])
         remanats[chemical] = reactions[chemical][0] * times - amount
-        carry = (qty * times) - remanats[reactant]
-        getCount(reactant, carry)
-
+        forward = (qty * times) - remanats[reactant]
+        getCount(reactant, forward)
 getCount('FUEL', 1)
 print(ore_count)
 
@@ -45,4 +43,47 @@ print(ore_count)
 
 total_ore = 1000000000000    # 1 Trillion
 
+# One approach would be brute force  
+# Trying fuels until we get the required total number of ores
+# Took nearly 10 hours on my computer
 
+i = 1
+# while True:
+#     ore_count = 0
+#     getCount('FUEL', i)
+#     if ore_count > total_ore:
+#         print(f'FUEL: {i - 1}')
+#         break
+#     elif ore_count == total_ore:
+#         print(f'FUEL: {i}')
+#         break
+#     if i % 1000 == 0:
+#         print(ore_count)
+#     i += 1
+
+# USING BINARY SEARCH
+
+while True:
+    ore_count = 0
+    getCount('FUEL', i)
+    print(f"{i} FUEL requires {ore_count} ORE")
+    if ore_count > total_ore:
+        break
+    mintry = i
+    i *= 2
+    maxtry = i
+
+print('STEP 2')
+
+while maxtry - mintry > 1:
+    print(f"answer is between {mintry} and {maxtry}")
+    newtry = (mintry + maxtry) // 2
+    ore_count = 0
+    getCount('FUEL', newtry)
+    print(f"{newtry} FUEL requires {ore_count} ORE")
+    if ore_count > total_ore:
+        maxtry = newtry
+    else:
+        mintry = newtry
+
+print(mintry)
